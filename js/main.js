807b84b2aafe2971e429a28f6da4e578f6b0043d
giuseppeesposito22 <gius.espo22@gmail.com>
2025-04-09 15:12:13 +0200

    add light on and light off

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log("ciao");
+const lightOnEl = document.getElementById("light-on");
+const lightOffEl = document.getElementById("light-off");
+const buttonLightEl = document.getElementById("button-light");
+
+console.log(lightOffEl.className.includes("d-inline-block"));
+
+buttonLightEl.addEventListener("click", () => {
+  if (lightOffEl.className === "") {
+    lightOnEl.className = "d-inline-block";
+    buttonLightEl.textContent = "spegni";
+    lightOffEl.className = "d-none";
+  } else {
+    lightOnEl.className = "d-none";
+    buttonLightEl.textContent = "accendi";
+    lightOffEl.className = "";
+  }
+});
